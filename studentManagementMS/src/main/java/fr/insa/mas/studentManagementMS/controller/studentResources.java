eff2d8bee288a7cf7c8725be66ce4f72c1461914
Vasseur Cyril <vasseu@insa-21015>
2024-11-25 16:42:09 +0100

    Springboot Init

---
+++ studentManagementMS/src/main/java/fr/insa/mas/studentManagementMS/controller/studentResources.java
@@ -0,0 +1,20 @@
+package fr.insa.mas.studentManagementMS.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import fr.insa.mas.studentManagementMS.model.Student;
+
+@RestController
+public class studentResources {
+	@GetMapping("/students")
+	public int studentNumber() {
+		return 200;
+	}
+	@GetMapping(value="/students/{id}")
+	public Student infosStudent(@PathVariable int id) {
+		Student student=new Student (id, "Paul", "Cyril");
+		return student;
+	}
+}
